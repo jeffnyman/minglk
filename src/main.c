c340ff843f06c1ec804ae3c1fd6e32941c3da828
Jeff Nyman <jeffnyman@gmail.com>
2024-10-14 11:51:14 -0500

    Add version info.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "glk.h"
+#include "minglk.h"
 
 int main(int argc, char *argv[])
 {
-  printf("MinGlk - Minimal Glk Library\n\n");
+  printf("MinGlk - Minimal Glk Library (v%s)\n\n", LIBRARY_VERSION);
 
   glk_main();
 
---
+++ src/minglk.h
@@ -0,0 +1,6 @@
+#ifndef MINGLK_H
+#define MINGLK_H
+
+#define LIBRARY_VERSION "0.1.0"
+
+#endif
