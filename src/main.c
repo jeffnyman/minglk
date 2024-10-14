0b36bd62977365863cfbb79186d9d7741b06d8b9
Jeff Nyman <jeffnyman@gmail.com>
2024-10-14 15:09:21 -0500

    Add type size checks.

--- src/main.c
+++ src/main.c
@@ -5,6 +5,30 @@
 
 int main(int argc, char *argv[])
 {
+  if (sizeof(glui32) != 4)
+  {
+    printf("Compile-time error: glui32 is not a 32-bit value.\n");
+    return EXIT_FAILURE;
+  }
+
+  if ((glui32)(-1) < 0)
+  {
+    printf("Compile-time error: glui32 is not unsigned.\n");
+    return EXIT_FAILURE;
+  }
+
+  if (sizeof(glsi32) != 4)
+  {
+    printf("Compile-time error: glsi32 is not a 32-bit value.\n");
+    return EXIT_FAILURE;
+  }
+
+  if ((glsi32)(-1) > 0)
+  {
+    printf("Compile-time error: glsi32 is not signed.\n");
+    return EXIT_FAILURE;
+  }
+
   printf("MinGlk - Minimal Glk Library (v%s)\n\n", LIBRARY_VERSION);
 
   glk_main();
