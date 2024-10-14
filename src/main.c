1ba649aba4a8223d9d4eaadd611b28f1569b5a65
Jeff Nyman <jeffnyman@gmail.com>
2024-10-14 11:02:05 -0500

    Add call to Glk entry point.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "glk.h"
 
 int main(int argc, char *argv[])
 {
   printf("MinGlk - Minimal Glk Library\n\n");
+
+  glk_main();
 
   return EXIT_SUCCESS;
 }
